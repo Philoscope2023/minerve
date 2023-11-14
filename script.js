29cb4e115bc50bf272f7b2bbbf7fe23d86fdd1af
Philoscope <150458838+Philoscope2023@users.noreply.github.com>
2023-11-14 05:56:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,51 +4,45 @@ document.addEventListener("DOMContentLoaded", function () {
     const resetIcon = document.getElementById("reset-icon");
     const ellipsisContainer = document.getElementById("ellipsis-container");
 
-    // Liste des questions possibles
     const questions = ["Question 1", "Question 2", "Question 3"];
-    
-    // Événement clic sur les options
+
     optionsContainer.addEventListener("click", function (event) {
         if (event.target.classList.contains("option")) {
-            displayResponse(event.target.textContent);
+            const selectedQuestion = event.target.textContent;
+            displayResponse(selectedQuestion); // Appel de la fonction pour afficher la réponse
             event.target.classList.add("clicked");
-            event.target.style.pointerEvents = "none"; // Désactive le clic après sélection
+            event.target.style.pointerEvents = "none";
         }
     });
 
-    // Événement clic sur les points de suspension (retour à la conversation précédente)
     ellipsisContainer.addEventListener("click", function () {
         clearResponse();
         optionsContainer.querySelectorAll(".option").forEach(option => {
             option.classList.remove("clicked");
-            option.style.pointerEvents = "auto"; // Réactive les clics
+            option.style.pointerEvents = "auto";
         });
     });
 
-    // Événement clic sur l'icône de réinitialisation
     resetIcon.addEventListener("click", function () {
         clearConversation();
     });
 
-    // Fonction pour afficher la réponse dans l'animation
     function displayResponse(answer) {
         const responseElement = document.createElement("div");
         responseElement.classList.add("response");
         responseElement.textContent = answer;
         conversationContainer.appendChild(responseElement);
-        conversationContainer.scrollTop = conversationContainer.scrollHeight; // Fait défiler vers le bas
+        conversationContainer.scrollTop = conversationContainer.scrollHeight;
     }
 
-    // Fonction pour effacer la réponse actuelle
     function clearResponse() {
         const responses = document.querySelectorAll(".response");
         responses[responses.length - 1].classList.add("fade-out");
         setTimeout(() => {
             responses[responses.length - 1].remove();
-        }, 500); // Temps de l'animation fade-out
+        }, 500);
     }
 
-    // Fonction pour réinitialiser toute la conversation
     function clearConversation() {
         conversationContainer.innerHTML = "";
         optionsContainer.querySelectorAll(".option").forEach(option => {
